06fa064d774e40b1b4392ee00192510dcaa5daee
Brian Ramirez <brians@uninorte.edu.co>
2019-11-27 23:13:13 -0500

    feat: Add problem description to minimum-diff-sum

--- minimum-diff-sum.js
+++ minimum-diff-sum.js
@@ -1,15 +1,20 @@
+/**
+ * Problem description:
+ * Partition a set into two subsets such that the difference of subset sums is
+ * minimum.
+ */
 function findMinimumDiff(arr, remaining, accrued, size, maxSize) {
   if (size + 1 <= maxSize) {
     remaining.forEach((n, index) => {
       const cloneArr = remaining.slice();
       cloneArr.splice(index, 1);
-                findMinimumDiff([...arr, n], cloneArr, accrued + n, size + 1, maxSize);
+      findMinimumDiff([...arr, n], cloneArr, accrued + n, size + 1, maxSize);
     });
   }
     if (arr.length > 0) {
-      console.log('A', arr);
-      console.log('b', remaining);
-      min = Math.min(Math.abs(accrued - remaining.reduce((a, b) => a + b, 0)), min);
+      min = Math.min(
+        Math.abs(accrued - remaining.reduce((a, b) => a + b, 0)), min
+      );
     }
   }
   var min = Infinity;
