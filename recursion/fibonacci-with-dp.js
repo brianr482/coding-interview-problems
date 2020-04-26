f714b7b076538bca458d7b7706f15ca3edd1a363
Brian Ramirez <brians@uninorte.edu.co>
2020-04-25 20:38:48 -0500

    refactor: Update fibonacci with dp solution

--- recursion/fibonacci-with-dp.js
+++ recursion/fibonacci-with-dp.js
@@ -9,19 +9,18 @@
  * @return {number}
  */
 function fibonacciDp(n) {
-  if (n === 0) {
-    return 0;
+  const dp = [0, 1];
+  if (n < 2) {
+    return dp[n + 1];
   }
 
-  if (n == 1) {
-    return 1;
+  if (n === 1) {
+    return 1
   }
-
-  dp = [0, 1];
-  let total = 0;
-  for (let i = 2; i < n; i++) {
-    total += dp[i - 1] + dp[i - 2];
-    dp.push(total);
+  
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
-  return total;
+  
+  return dp[n];
 } 
